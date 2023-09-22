9140373045a6cf1ce64617d7a121845d94c6d6f5
kaia.ai <33589365+kaiaai@users.noreply.github.com>
2023-09-22 00:42:44 -0700

    Update urdf2sdf.sh

--- kaiaai_gazebo/script/urdf2sdf.sh
+++ kaiaai_gazebo/script/urdf2sdf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# TODO syntax urdf2sdf.sh /full/path/robot.urdf.xacro
-# TODO syntax urdf2sdf.sh /full/path/robot.urdf  # Don't run xacro
+# syntax urdf2sdf.sh /full/path/robot.urdf.xacro
+# TODO syntax urdf2sdf.sh /full/path/robot.urdf  # Skip running xacro
 [[ -z "$1" ]] && { echo "$1 does not exist" ; exit 1; }
 cd $1
 
@@ -9,9 +9,7 @@ cd $1
 
 xacro $2.urdf.xacro > $2.urdf
 gz sdf -p $2.urdf > $2.sdf
-sed_arg="s/_description\/sdf\/$2//g"
-sed -i $sed_arg $2.sdf
+# sed_arg="s/_description\/sdf\/$2//g"
+# sed -i $sed_arg $2.sdf
 rm $2.urdf
 mv $2.sdf ../sdf/$2/model.sdf
-
-
